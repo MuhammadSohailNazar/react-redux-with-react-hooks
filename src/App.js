e3aaea45d1fee30ee1280462de3cbc8906fd22e6
msohail@sastaticket.pk <nazar@mailcohen.com>
2021-03-13 15:24:15 +0500

    1. create react app 2. removing extra file for cra 3. add redux and react-redux 4. react redux example with class component.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ClassComponent from "./components/classComponent";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div style={{ padding: "50px" }}>
+        <h3> React Class </h3>
+        <ClassComponent />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
